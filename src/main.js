0d9413dea9e0ea6d949732500319f47e562b6252
Nuri <72916917+npuigdemont@users.noreply.github.com>
2022-05-02 15:15:40 +0200

    exercici1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div class="m-3">
+      <h4>Què vols fer?</h4>
+             <input type="checkbox" id="web" value=500 v-model.number="opcions" class="m-1">
+             <label for="web"> Una pàgina web (500 €)</label>    
+              <br>
+             <input type="checkbox" id="seo" value=300 v-model.number="opcions" class="m-1">
+             <label for="seo"> Una consultoria SEO (300 €)</label>  
+              <br>
+             <input type="checkbox" id="ads" value=200 v-model.number="opcions" class="m-1">
+             <label for="ads"> Una campanya de Google Ads (200 €)</label>    
+      
+       
+       <div>Preu:  <strong>{{ sum }}</strong> euros</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  
+    data() {
+      return {
+        opcions: []
+        } 
+    },
+    computed: {
+        sum() {
+          var total = 0
+          return this.opcions.reduce((a,b)=> (a+b),0)
+        }
+      }
 }
+    
+
 </script>
+
+<style scoped>
+
+</style>
