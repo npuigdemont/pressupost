220de0f71d1f09f11e81446187d8bff59a2ee4a3
Nuri <72916917+npuigdemont@users.noreply.github.com>
2022-05-05 19:25:55 +0200

    exercici6

--- src/components/PressupostList.vue
+++ src/components/PressupostList.vue
@@ -1,14 +1,35 @@
 <template>
-    <div>
-        <h4>Pressupost:</h4>
-        
-        <h6>Budget: {{budget}}</h6>
+    <div class="m-4">
+        <h4>Pressupostos:</h4>
+
+        <ul v-for="(pres, index) in budget" :key="index">
+            <li>Client: {{pres.client}}</li>
+            <li>Pressupost: {{pres.pressupost}}</li>
+            <li v-if="pres.opcions[0] == 500">Pàgina web || Quantitat de pàgines: {{pres.pag}} || Quantitat d'idiomes: {{pres.lang}}</li>
+            <li v-if="pres.opcions[0] == 300 || pres.opcions[1] == 300">Consultoria SEO</li>
+            <li v-if="pres.opcions[0] == 200 || pres.opcions[1] == 200 || pres.opcions[3] == 200">Campanya de Google Adds</li>
+            
+            <li>Total: {{pres.total}}</li>
+            <li>Data: {{pres.data}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
     name: 'PressupostList',
-    props: ['budget']
+    props: ['budget'],
+
 }
 </script>
+
+<style scoped>
+ul {
+    border: 2px solid black;
+}
+li {
+    list-style: none;
+    text-decoration: none;
+    align-content: left;
+}
+</style>
